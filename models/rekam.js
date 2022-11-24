46864b4a5f56f25525dd6459cfa174aa49f7b71c
Susilowati12 <susilowatidadapayam@gmail.com>
2022-11-24 08:00:38 +0700

    first

---
+++ models/rekam.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose
+
+const rekamSchema = new Schema({
+    id_konsultasi:
+    {
+        type: Schema.Types.ObjectId,
+        ref: "Konsultasi",
+    },
+    anamnesis: String,
+    diagnosis: String,
+    obat: String,
+})
+
+const Rekam = mongoose.model("rekam", rekamSchema)
+
+module.exports = Rekam
