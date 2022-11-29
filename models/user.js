bbcecbb47f7e18ef814d48a38dfa3494e6de328b
Susilowati12 <susilowatidadapayam@gmail.com>
2022-11-29 11:25:33 +0700

    tambahkan kode respon

--- models/user.js
+++ models/user.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const User = mongoose.model(
   "User",
   new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+    },
     email: {
       type: String,
       required: true,
